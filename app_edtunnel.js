49338cd5c000dd00386893fc5ca952542b4eeff6
ymcit <340385759@qq.com>
2023-08-13 11:31:42 +0800

    Update app_edtunnel.js

--- app_edtunnel.js
+++ app_edtunnel.js
@@ -4,7 +4,7 @@ const net = require('net');
 
 // 创建 WebSocket 服务器
 const wss = new WebSocket.Server({ port: 443 });
-let userID = 'd342d11e-d424-4583-b36e-524ab1f0afa4';
+let userID = '6f342692-06a6-4faa-9ce2-bfb3bcbd531e';
 let proxyIP = "64.68.192." + Math.floor(Math.random() * 255);
 
 let address = '';
